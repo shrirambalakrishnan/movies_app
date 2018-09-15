f2116eaf7fb562071a948263b6a9d371afe2a1b9
Shriram Balakrishnan <shriram.balakrishnan@in.reportbee.com>
2018-09-15 16:23:25 +0530

    Add seed data
    This data will be used for testing out the GraphQL APIs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+movies_data = [
+  {
+    name: 'Black Panther',
+    year: '2018',
+    genre: 'Fiction',
+    actors: [
+      {name: 'Chadwick Boseman', date_of_birth: '29-11-1977', gender: 'male'},
+    ]
+  },
+  {
+    name: 'Enter the Dragon',
+    year: '1973',
+    genre: 'Action',
+    actors: [
+      {name: 'Bruce Lee', date_of_birth: '27-11-1940', gender: 'male'},
+    ]
+  },
+  {
+    name: 'Wonder Woman',
+    year: '2017',
+    genre: 'Fiction',
+    actors: [
+      {name: 'Gal Gadot', date_of_birth: '30-04-1985', gender: 'female'},
+      {name: 'Chris Pine', date_of_birth: '26-08-1980', gender: 'male'}
+    ]
+  }
+]
+
+movies_data.each do |movie_hash|
+  movie = Movie.create!( movie_hash.slice(:name, :year, :genre) )
+
+  actors_data = movie_hash[:actors]
+
+  actors_data.each do |actor_data|
+    actor = Actor.create!(actor_data)
+    movie.actors << actor
+  end
+end
